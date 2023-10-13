072a72072fe12705d5047a672f8793d17b6c3e9b
Sheggsmann <promisesheggs@gmail.com>
2023-10-13 14:07:54 -0700

    fix: redirect user to logout on invalid error

--- src/api/apiClient.ts
+++ src/api/apiClient.ts
@@ -1,5 +1,7 @@
 import axios, { AxiosInstance, isAxiosError } from "axios";
 import toast from "react-hot-toast";
+import { useAppDispatch } from "../redux/hooks";
+import { authActions } from "../redux/slices/authSlice";
 
 export const apiClient: AxiosInstance = axios.create({
   baseURL: `${import.meta.env.VITE_BACKEND_URL}`,
@@ -23,11 +25,18 @@ apiClient.interceptors.response.use(
   },
   (error) => {
     if (isAxiosError(error)) {
-      console.log(error.response?.data);
+      // console.log(error.response?.data);
       if (error.response?.data?.message === "Vaidation Failure") {
         toast.error(Object.values(error.response?.data?.validationErrors as object)[0], {
           position: "top-right",
         });
+      }
+
+      if (error.response?.status === 401) {
+        toast.error("Invalid Token", { position: "top-right" });
+
+        const dispatch = useAppDispatch();
+        dispatch(authActions.logout());
       }
     }
     return Promise.reject(error);
